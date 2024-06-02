fd0853bf663bc720ec7397cb4646d0cc493fffe4
Ericode254 <jilloerick6@gmail.com>
2024-06-02 09:36:55 +0300

    improved the last implimentation

--- queue.c
+++ queue.c
@@ -1,54 +1,75 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 // NOTE: The size of the queue can also be dynamically allocated during runtime
 #define MAX 100
 
 char Queue[MAX];
-int spos = 0;
-int rpos = 0;
+int rear = -1;
+int front = -1;
  
 // function declarations
-void qstore(char value);
-char qretrieve();
+void Enqueue(char value);
+char Dequeue();
 void printQueue();
+int isEmpty();
+int Front();
 
 int main()
 {
-  qstore('A');
-  qstore('B');
-  qstore('C');
-  qretrieve();
+  Enqueue('A');
+  Enqueue('B');
+  Enqueue('C');
+  Dequeue();
   
   printQueue();
+  // printf("%c\n", Front());
 
   return 0;
 }
 
+// a function for checking whether the queue is empty
+int isEmpty() {
+  if (front == -1 && rear == -1) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
 // a function for adding an item into the queue
-void qstore(char value) {
-  if (spos == MAX) {
+void Enqueue(char value) {
+  if ((rear+1) % MAX == front) {
     printf("The queue is full\n");
     return;
+  } else if (isEmpty()) {
+    front = 0;
+    rear = 0;
+  } else {
+    rear = (rear + 1) % MAX;
   }
 
-  Queue[spos] = value;
-  spos++;
+  Queue[rear] = value;
 }
 
 // a function for removing an item from the queue
-char qretrieve() {
-  if (rpos == spos) {
+char Dequeue() {
+  if (isEmpty()) {
     printf("The queue is empty");
+  } else if (front == rear) {
+    front = -1;
+    rear = -1;
   }
 
-  rpos++;
-  return Queue[rpos-1];
+  front = (front + 1) % MAX;
+  return Queue[front-1];
 }
 
+int Front() {
+  return Queue[front];
+}
 // a function for printing what is in the queue
 void printQueue() {
-  for (int i = rpos; i < spos; i++) {
+  for (int i = front; i <= rear; i++) {
     printf("%c ", Queue[i]);
   }
 }
